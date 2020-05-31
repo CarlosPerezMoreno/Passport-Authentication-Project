0f0b736525904412bcee4986b4651fcef52bb92a
Carlos <carlos.perez.moreno.02@gmail.com>
2020-05-31 17:34:53 +0200

    feat: Styles configured. Register and Login forms added.
    Config/keys file added, which contains the URL to MongoDB cluster.
    User file added, which contains the Schema module for user registration
    fields. App now has a express a body parser. Routes/users now has
    conditionals and checks for registration, but doesn't work properly.
    There is a bug that need to be fixed.

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  MongoURI:
+    "mongodb+srv://CarlosP:12345@cluster0-qq9qk.mongodb.net/test?retryWrites=true&w=majority",
+};
--- routes/users.js
+++ routes/users.js
@@ -2,9 +2,43 @@ const express = require("express");
 const router = express.Router();
 
 //Login page
-router.get("/login", (req, res) => res.send("Login"));
+router.get("/login", (req, res) => res.render("login"));
 
 //Register page
-router.get("/register", (req, res) => res.send("Register"));
+router.get("/register", (req, res) => res.render("register"));
+
+//Register Handle
+router.post("/register", (req, res) => {
+  const { name, email, password, passwordTwo } = req.body;
+
+  let errors = [];
+
+  //Check required fields
+  if (!name || !email || !password || !passwordTwo) {
+    errors.push({ msg: "Make sure to fill all fields" });
+  }
+
+  //Passwords match?
+  if (password != passwordTwo) {
+    errors.push({ msg: "Both passwords are not matching" });
+  }
+
+  //Check the pass length
+  if (password.length < 6) {
+    errors.push({ msg: "Your password must have at least 6 characters" });
+  }
+
+  if (errors.length > 0) {
+    res.render("register", {
+      errors,
+      name,
+      email,
+      password,
+      passwordTwo,
+    });
+  } else {
+    res.send("pass");
+  }
+});
 
 module.exports = router;
